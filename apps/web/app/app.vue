9714994431b8dad5f50cc697de93fc1df8723ce0
Ronaldo Hortmann <ronaldohortmann02@gmail.com>
2026-04-11 14:30:11 -0300

    initialized nuxt 4

---
+++ apps/web/app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+console.log('teste', appConfig);
+</script>
